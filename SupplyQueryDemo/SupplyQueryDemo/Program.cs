b764eeba1356af36e5d062a6c6b0be2ed16a9cee
Alex Kubiesa <alex.kubiesa@altium.com>
2022-03-09 10:53:17 +0000

    Add supply query demo.

---
+++ SupplyQueryDemo/SupplyQueryDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using SupplyQueryDemo;
+
+const string QueryTemplate = @"
+query Search {{
+  supSearchMpn(q: ""{0}"", limit: 2) {{
+    results {{
+      part {{
+        mpn
+        shortDescription
+        manufacturer {{
+          name
+        }}
+      }}
+    }}
+  }}
+}}";
+
+while (true)
+{
+    // prompt for an MPN to search
+    Console.Write("Search MPN: ");
+    var mpn = Console.ReadLine();
+    if (string.IsNullOrEmpty(mpn))
+        return;
+
+    using HttpClient supplyClient = await SupplyClient.GetClientAsync();
+
+    // substitute the MPN into the query
+    string query = string.Format(QueryTemplate, mpn);
+
+    // run the query
+    Response result = await supplyClient.RunQueryAsync(query);
+
+    if (result.Data?.SupSearchMpn?.Results == null)
+    {
+        continue;
+    }
+
+    // print the results
+    foreach (var it in result.Data.SupSearchMpn.Results)
+    {
+        Console.WriteLine($"MPN: {it?.Part?.Mpn}");
+        Console.WriteLine($"Desciption: {it?.Part?.ShortDescription}");
+        Console.WriteLine($"Manufacturer: {it?.Part?.Manufacturer?.Name}");
+        Console.WriteLine();
+    }
+}
+
---
+++ SupplyQueryDemo/SupplyQueryDemo/SupplyClient.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Nexar.Client.Token;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplyQueryDemo
+{
+    internal static class SupplyClient
+    {
+        // access tokens expire after one day
+        private static readonly TimeSpan tokenLifetime = TimeSpan.FromDays(1);
+
+        // assume Nexar client ID and secret are set as environment variables
+        private static readonly string clientId = Environment.GetEnvironmentVariable("NEXAR_CLIENT_ID") ?? throw new InvalidOperationException("Please set environment variable 'NEXAR_CLIENT_ID'");
+        private static readonly string clientSecret = Environment.GetEnvironmentVariable("NEXAR_CLIENT_SECRET") ?? throw new InvalidOperationException("Please set environment variable 'NEXAR_CLIENT_SECRET'");
+
+        // keep track of token and expiry time
+        private static string? token = null;
+        private static DateTime tokenExpiresAt = DateTime.MinValue;
+
+        internal static async Task<HttpClient> GetClientAsync()
+        {
+            // get an access token, or get a new one if it expired
+            if (token == null || DateTime.UtcNow >= tokenExpiresAt)
+            {
+                tokenExpiresAt = DateTime.UtcNow + tokenLifetime;
+                using HttpClient authClient = new();
+                token = await authClient.GetNexarTokenAsync(clientId, clientSecret);
+            }
+
+            // create and configure the supply client
+            HttpClient supplyClient = new()
+            {
+                BaseAddress = new Uri("https://api.nexar.com/graphql"),
+                DefaultRequestHeaders =
+                {
+                    Authorization = new AuthenticationHeaderValue("Bearer", token)
+                }
+            };
+
+            return supplyClient;
+        }
+
+        internal static async Task<Response> RunQueryAsync(this HttpClient supplyClient, string query)
+        {
+            HttpResponseMessage httResponse = await supplyClient.PostAsync(supplyClient.BaseAddress, new StringContent(query, Encoding.UTF8, "application/json"));
+            httResponse.EnsureSuccessStatusCode();
+            string responseString = await httResponse.Content.ReadAsStringAsync();
+            Response response = JsonConvert.DeserializeObject<Response>(responseString);
+            return response;
+        }
+    }
+}
