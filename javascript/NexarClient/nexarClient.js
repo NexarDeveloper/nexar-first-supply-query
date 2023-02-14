f10840df80fd0f7120462077a2088c55bb777077
Rowan Murray <67557980+RowMur@users.noreply.github.com>
2023-02-14 09:58:20 +0000

    redirect at api.nexar.com

--- javascript/NexarClient/nexarClient.js
+++ javascript/NexarClient/nexarClient.js
@@ -2,7 +2,7 @@ const { URLSearchParams } = require("url");
 const https = require("https");
 
 const TOKEN_OPTIONS = {
-  hostname: "identity.nexaruat.com",
+  hostname: "identity.nexar.com",
   path: "/connect/token",
   method: "POST",
   headers: {
@@ -59,7 +59,7 @@ class NexarClient {
   #exp;
   #id;
   #secret;
-  hostName = "api.nexaruat.com";
+  hostName = "api.nexar.com";
 
   /**
    * Client for the Nexar API to manage authorization and requests.
