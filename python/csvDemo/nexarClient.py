381cf2c2aad5312cdf3c40cc479fcbc04450e46a
Rowan Murray <67557980+RowMur@users.noreply.github.com>
2023-02-24 10:34:22 +0000

    Add demo for working with csv files

---
+++ python/csvDemo/nexarClient.py
@@ -0,0 +1,73 @@
+"""Resources for making Nexar requests."""
+import requests
+import base64
+import json
+import time
+from typing import Dict
+
+NEXAR_URL = "https://api.nexar.com/graphql"
+PROD_TOKEN_URL = "https://identity.nexar.com/connect/token"
+
+def get_token(client_id, client_secret):
+    """Return the Nexar token from the client_id and client_secret provided."""
+
+    if not client_id or not client_secret:
+        raise Exception("client_id and/or client_secret are empty")
+
+    token = {}
+    try:
+        token = requests.post(
+            url=PROD_TOKEN_URL,
+            data={
+                "grant_type": "client_credentials",
+                "client_id": client_id,
+                "client_secret": client_secret
+            },
+            allow_redirects=False,
+        ).json()
+
+    except Exception:
+        raise
+
+    return token
+def decodeJWT(token):
+    return json.loads(
+        (base64.urlsafe_b64decode(token.split(".")[1] + "==")).decode("utf-8")
+    )
+
+class NexarClient:
+    def __init__(self, id, secret) -> None:
+        self.id = id
+        self.secret = secret
+        self.s = requests.session()
+        self.s.keep_alive = False
+
+        self.token = get_token(id, secret)
+        self.s.headers.update({"token": self.token.get('access_token')})
+        self.exp = decodeJWT(self.token.get('access_token')).get('exp')
+
+    def check_exp(self):
+        if (self.exp < time.time() + 300):
+            self.token = get_token(self.id, self.secret)
+            self.s.headers.update({"token": self.token.get('access_token')})
+            self.exp = decodeJWT(self.token.get('access_token')).get('exp')
+
+    def get_query(self, query: str, variables: Dict) -> dict:
+        """Return Nexar response for the query."""
+        try:
+            self.check_exp()
+            r = self.s.post(
+                NEXAR_URL,
+                json={"query": query, "variables": variables},
+            )
+
+        except Exception as e:
+            print(e)
+            raise Exception("Error while getting Nexar response")
+
+        response = r.json()
+        if ("errors" in response):
+            for error in response["errors"]: print(error["message"])
+            raise SystemExit
+
+        return response["data"]
---
+++ python/csvDemo/program.py
@@ -0,0 +1,80 @@
+import os, csv
+from nexarClient import NexarClient
+
+mpns = []
+
+with open("input.csv", newline="") as inputFile:
+    data = csv.reader(inputFile, delimiter=" ", quotechar="|")
+    for row in data:
+        mpns += row[0].split(",")
+
+gqlQuery = '''
+query csvDemo ($queries: [SupPartMatchQuery!]!) {
+  supMultiMatch (
+    currency: "EUR",
+    queries: $queries
+  ){
+    parts {
+      mpn
+      name
+      sellers {
+        company {
+          id
+          name
+        }
+        offers {
+          inventoryLevel
+          prices {
+            quantity
+            convertedPrice
+            convertedCurrency
+          }
+        }
+      }
+    }
+  }
+}
+'''
+
+if __name__ == "__main__":
+
+    clientId = os.environ["NEXAR_CLIENT_ID"]
+    clientSecret = os.environ["NEXAR_CLIENT_SECRET"]
+    nexar = NexarClient(clientId, clientSecret)
+
+    queries = []
+    for mpn in mpns:
+        queries += [{"start": 1, "limit": 1, "mpn": mpn}]
+
+    variables = {
+        "queries": queries
+    }
+    results = nexar.get_query(gqlQuery, variables)
+
+    if results:
+        with open("output.csv", "w", newline="") as outputFile:
+            writer = csv.writer(outputFile)
+
+            for query in results.get("supMultiMatch"):
+                writer.writerow(["MPN","Name"])
+                writer.writerow([query.get("parts")[0].get("mpn"),query.get("parts")[0].get("name")])
+                writer.writerow("")
+
+                for seller in query.get("parts",{})[0].get("sellers"):
+                    writer.writerow(["Seller ID","Seller Name"])
+                    writer.writerow([seller.get("company").get("id"),seller.get("company").get("name")])
+                    writer.writerow("")
+
+                    for offer in seller.get("offers"):
+                        writer.writerow(["Stock",offer.get("inventoryLevel")])
+                        writer.writerow("")
+
+                        writer.writerow(["Quantity","Price"])
+                        for price in offer.get("prices"):
+                            writer.writerow([price.get("quantity"), price.get("convertedPrice")])
+                        
+                        writer.writerow("")
+
+                    writer.writerow("")
+                
+                writer.writerow("")
